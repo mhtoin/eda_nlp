a45693900274ad0847270b2fb697c614a36707ed
Miika <mhtoin@utu.fi>
2020-03-02 12:31:56 +0200

    Some cleaning up

--- code/character_eda.py
+++ code/character_eda.py
@@ -8,55 +8,6 @@ import string
 
 random.seed(1)
 
-# stop words list
-stop_words = ['i', 'me', 'my', 'myself', 'we', 'our',
-              'ours', 'ourselves', 'you', 'your', 'yours',
-              'yourself', 'yourselves', 'he', 'him', 'his',
-              'himself', 'she', 'her', 'hers', 'herself',
-              'it', 'its', 'itself', 'they', 'them', 'their',
-              'theirs', 'themselves', 'what', 'which', 'who',
-              'whom', 'this', 'that', 'these', 'those', 'am',
-              'is', 'are', 'was', 'were', 'be', 'been', 'being',
-              'have', 'has', 'had', 'having', 'do', 'does', 'did',
-              'doing', 'a', 'an', 'the', 'and', 'but', 'if', 'or',
-              'because', 'as', 'until', 'while', 'of', 'at',
-              'by', 'for', 'with', 'about', 'against', 'between',
-              'into', 'through', 'during', 'before', 'after',
-              'above', 'below', 'to', 'from', 'up', 'down', 'in',
-              'out', 'on', 'off', 'over', 'under', 'again',
-              'further', 'then', 'once', 'here', 'there', 'when',
-              'where', 'why', 'how', 'all', 'any', 'both', 'each',
-              'few', 'more', 'most', 'other', 'some', 'such', 'no',
-              'nor', 'not', 'only', 'own', 'same', 'so', 'than', 'too',
-              'very', 's', 't', 'can', 'will', 'just', 'don',
-              'should', 'now', '']
-
-# cleaning up text - not sure if this is needed or if
-# some other sort of cleaning up is needed
-import re
-
-
-def get_only_chars(line):
-    clean_line = ""
-
-    line = line.replace("’", "")
-    line = line.replace("'", "")
-    line = line.replace("-", " ")  # replace hyphens with spaces
-    line = line.replace("\t", " ")
-    line = line.replace("\n", " ")
-    #line = line.lower()
-
-    for char in line:
-        if char in 'qwertyuiopasdfghjklzxcvbnmåäöQWERTYUIOPÅASDFGHJKLÖÄZXCVBNM ':
-            clean_line += char
-        else:
-            clean_line += ' '
-
-    clean_line = re.sub(' +', ' ', clean_line)  # delete extra spaces
-    if clean_line[0] == ' ':
-        clean_line = clean_line[1:]
-    return clean_line
-
 
 ########################################################################
 # Random deletion
